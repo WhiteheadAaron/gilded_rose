b7d642dd59652f9d75b4b740f86718677e1cc148
Aaron <whitehead.aaron@live.com>
2021-02-10 19:26:58 -0700

    Cleaned up all code in gilded_rose.js to be easier to read and understand. Also wrote tests for each of the products.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,146 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  // DEXTERITY VEST TESTS
+  it("Should lower dexterity vests quality with shelf life remaining by 1", function () {
+    items = [new Item('+5 Dexterity Vest', 5, 10)];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(9);
+    expect(items[0].sell_in).toEqual(4);
+  });
+
+  it("Should lower dexterity vests quality with NO shelf life remaining by 2", function () {
+    items = [new Item('+5 Dexterity Vest', 0, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+    expect(items[0].sell_in).toEqual(-1);
+  });
+
+  it("Should not allow dexterity vests quality to go below 0", function () {
+    items = [new Item('+5 Dexterity Vest', -10, 1)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+    expect(items[0].sell_in).toEqual(-11);
+  });
+
+
+  // ELIXIR OF THE MONGOOSE TESTS
+  it("Should lower elixir quality with shelf life remaining by 1", function () {
+    items = [new Item('Elixir of the Mongoose', 5, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(9);
+    expect(items[0].sell_in).toEqual(4);
+  });
+
+  it("Should lower elixir quality with NO shelf life remaining by 2", function () {
+    items = [new Item('Elixir of the Mongoose', 0, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+    expect(items[0].sell_in).toEqual(-1);
+  });
+
+  it("Should not allow elixir quality to go below 0", function () {
+    items = [new Item('Elixir of the Mongoose', -10, 1)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+    expect(items[0].sell_in).toEqual(-11);
+  });
+
+
+  // AGED BRIE TESTS
+  // ** These are based on my assumption in gilded_rose.js that the value rises by 1 in perpetuity **
+  it("Should raise aged brie quality by 1", function () {
+    items = [new Item('Aged Brie', 5, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(11);
+    expect(items[0].sell_in).toEqual(4);
+  });
+
+  it("Should raise aged brie quality with NO shelf life remaining by 1", function () {
+    items = [new Item('Aged Brie', 0, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(11);
+    expect(items[0].sell_in).toEqual(-1);
+  });
+
+  it("Should not allow aged brie quality to go above 50", function () {
+    items = [new Item('Aged Brie', 15, 50)];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+    expect(items[0].sell_in).toEqual(14);
+  });
+
+
+  // SULFURAS TESTS
+  it("Should not change value or shelf life of sulfuras legendary items", function () {
+    items = [new Item('Sulfuras, Hand of Ragnaros', 10, 80)];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+    expect(items[0].sell_in).toEqual(10);
+  });
+
+
+  // BACKSTAGE CONCERT PASSES TESTS
+  it("Should raise back stage passes quality by 1 when there are more than 10 days until the concert", function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(11);
+    expect(items[0].sell_in).toEqual(14);
+  });
+
+  it("Should raise back stage passes quality by 2 when there are between 6 and 10 days until the concert", function () {
+    items = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 6, 15),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 8)
+    ];
+    update_quality();
+    expect(items[0].quality).toEqual(17);
+    expect(items[0].sell_in).toEqual(5);
+
+    expect(items[1].quality).toEqual(10);
+    expect(items[1].sell_in).toEqual(9);
+  });
+
+  it("Should raise back stage passes quality by 3 when there are between 1 and 5 days until the concert", function () {
+    items = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 1, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 15)
+    ];
+    update_quality();
+    expect(items[0].quality).toEqual(23);
+    expect(items[0].sell_in).toEqual(0);
+
+    expect(items[1].quality).toEqual(18);
+    expect(items[1].sell_in).toEqual(4);
+  });
+
+  it("Should lower back stage passes quality to 0 when the concert has passed", function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+    expect(items[0].sell_in).toEqual(-1);
+  });
+
+
+  // CONJURED ITEMS TESTS
+  it("Should lower conjured items quality with shelf life remaining by 2", function () {
+    items = [new Item('Conjured Mana Cake', 3, 6)];
+    update_quality();
+    expect(items[0].quality).toEqual(4);
+    expect(items[0].sell_in).toEqual(2);
+  });
+
+  it("Should lower conjured items quality with NO shelf life remaining by 4", function () {
+    items = [new Item('Conjured Mana Cake', 0, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(6);
+    expect(items[0].sell_in).toEqual(-1);
+  });
+
+  it("Should not allow conjured items quality to go below 0", function () {
+    items = [new Item('Conjured Mana Cake', -1, 1)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+    expect(items[0].sell_in).toEqual(-2);
   });
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,53 +6,58 @@ function Item(name, sell_in, quality) {
 
 var items = [];
 
+function lower_date(i) {
+  items[i].sell_in--;
+}
+
+// This makes sure the quality never goes below 0
+function lower_quality(i, amount) {
+  items[i].quality = Math.max(items[i].quality - amount, 0);
+}
+
+// This makes sure the quality never goes above 50
+function raise_quality(i, amount) {
+  items[i].quality = Math.min(items[i].quality + amount, 50);
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          // if (items[i].sell_in < 11) {
-          //   if (items[i].quality < 50) {
-          //     items[i].quality = items[i].quality + 1
-          //   }
-          // }
-          if (items[i].sell_in <= 10 && items[i].quality < 50) items[i].quality = items[i].quality + 1;
-          console.log('hello there');
-          // if (items[i].sell_in < 6) {
-          //   if (items[i].quality < 50) {
-          //     items[i].quality = items[i].quality + 1
-          //   }
-          // }
-          if (items[i].sell_in < 6 && items[i].quality < 50) items[i].quality = items[i].quality + 1;
-        }
-      }
+
+    // This one is simple, lower by 1 value each day until the sell by date.
+    // It then lowers by 2 until it has no value remaining.
+    // Both the Dexterity Vest and the Elixir behave the same way, so I combined them for simplicity.
+    if (items[i].name == '+5 Dexterity Vest' || items[i].name == 'Elixir of the Mongoose') {
+      if (items[i].sell_in > 0) lower_quality(i, 1);
+      else lower_quality(i, 2);
+      lower_date(i);
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+
+    // I don't see any specification what happens after the sell by date reaches 0.
+    // With this code it will just continue to raise in value by 1 each day forever.
+    if (items[i].name == 'Aged Brie') {
+      raise_quality(i, 1);
+      lower_date(i);
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
+
+    // Do nothing because the item's value does not change, and the sell by date is not relevant.
+    if (items[i].name == 'Sulfuras, Hand of Ragnaros') { }
+
+    // The concert tickets have 4 different behaviors depending on the sell by date.
+    // I used conditionals to make sure that only one function would be executed on the quality level.
+    // I used the lower function (even though I could have just hard coded the value to 0) for consistency.
+    if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (items[i].sell_in <= 0) lower_quality(i, items[i].quality);
+      if (items[i].sell_in > 0 && items[i].sell_in <= 5) raise_quality(i, 3);
+      if (items[i].sell_in > 5 && items[i].sell_in <= 10) raise_quality(i, 2);
+      if (items[i].sell_in > 10) raise_quality(i, 1);
+      lower_date(i);
+    }
+
+    // The Conjured items behave the same as the Vest and the Elixir, just doubled.
+    if (items[i].name == 'Conjured Mana Cake') {
+      if (items[i].sell_in > 0) lower_quality(i, 2);
+      else lower_quality(i, 4);
+      lower_date(i);
     }
   }
 }
